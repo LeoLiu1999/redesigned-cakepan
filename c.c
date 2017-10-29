5e20a23e791096057ae0f61a5995da803a15dc52
TakingTheL <yliu6@stuy.edu>
2017-10-29 16:21:20 -0400

    read/write functionality partially done

--- c.c
+++ c.c
@@ -10,6 +10,7 @@ int genRand() {
   int fd = open("/dev/random", O_RDONLY);
   int p[1];
   read(fd, p, 4);
+  close(fd);
   return p[0];
 }
 
@@ -21,11 +22,36 @@ void printArr(int *arr, unsigned char length) {
   printf("%d]\n", arr[i+1]);
 }
 
+void writeArrToFile(int *arr){
+  int fd = open("filey.mcfileface", O_CREAT | O_RDWR, 0644);
+  write(fd, arr,10);
+  close(fd);
+}
+
+void readArrFromFile(int *arr){
+  int fd = open("filey.mcfileface", O_RDONLY);
+  read(fd, arr,10);
+  close(fd);
+}
+
 int main() {
-  int arr[10];
+  int arr1[10];
   int i = 0;
   for (; i<10; i++)
-    arr[i] = genRand();
-  printArr(arr, 10);
+    arr1[i] = genRand();
+  printArr(arr1, 10);
+
+  printf("Writing numbers to file...\n");
+  writeArrToFile(arr1);
+  printf("\n");
+  
+  printf("Reading numbers from file...\n");
+  int arr2[10];
+  readArrFromFile(arr2);
+  printf("\n");
+  
+  printf("Verification that written values were the same:\n");
+  printArr(arr2, 10);
+
   return 0;
 }
